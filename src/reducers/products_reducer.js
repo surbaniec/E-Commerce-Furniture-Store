8e13cccdc58e6606d2481b6d9c73657ec2da73a0
surbaniec <radsler1999@gmail.com>
2022-10-17 14:54:26 +0200

    add fetch products

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -21,6 +21,27 @@ const products_reducer = (state, action) => {
         ...state,
         isSidebarOpen: false,
       };
+    case GET_PRODUCTS_BEGIN:
+      return {
+        ...state,
+        products_loading: true,
+      };
+    case GET_PRODUCTS_SUCCESS:
+      const featured_products = action.payload.filter(
+        (product) => product.featured === true
+      );
+      return {
+        ...state,
+        products_loading: false,
+        products: action.payload,
+        featured_products,
+      };
+    case GET_PRODUCTS_ERROR:
+      return {
+        ...state,
+        products_loading: false,
+        products_error: true,
+      };
     default:
       return state;
   }
